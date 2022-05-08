bc5d8534e8dc4fba1c20f59401e7940def0040a4
Jesse Griffin <jag3773@gmail.com>
2022-05-07 21:24:36 -0400

    Fixed github urls, fixed docs to pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,17 +98,17 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
-        docs: {
+        pages: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/jag3773/ekfocus/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/jag3773/ekfocus/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
